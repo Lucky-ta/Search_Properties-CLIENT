1d04c4413cc77f0b07687f344535e96dcd2686b9
Lucas Maieski <oficiallucasmaieski@gmail.com>
2023-02-09 00:28:24 -0300

    Create register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,10 @@
+import { Search } from "components/data/Search";
+import { Layout } from "components/Layout";
+
+export default function Register() {
+  return (
+    <Layout>
+      <Search name="Registrar imóvel" />
+    </Layout>
+  );
+}
