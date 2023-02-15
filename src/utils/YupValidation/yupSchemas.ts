33fb208ecf53f578238c61c146d420d6733b93f1
Lucas Maieski <oficiallucasmaieski@gmail.com>
2023-02-15 01:46:15 -0300

    Crete custom error messages with yup

--- src/utils/YupValidation/yupSchemas.ts
+++ src/utils/YupValidation/yupSchemas.ts
@@ -1,16 +1,16 @@
 import * as Yup from 'yup';
 
 export const userSchema = Yup.object().shape({
-    name: Yup.string().min(3).required(),
-    email: Yup.string().email().required(),
-    password: Yup.string().min(6).required(),
+    name: Yup.string().min(3).required("Nome é um campo obrigatório"),
+    email: Yup.string().email().required("E-mail é um campo obrigatório"),
+    password: Yup.string().min(6).required("Senha é um campo obrigatório"),
 });
 
 export const propertySchema = Yup.object().shape({
-    name: Yup.string().min(3).required(),
-    propertyId: Yup.string().min(6).required(),
+    name: Yup.string().min(3).required("Nome é um campo obrigatório"),
+    propertyId: Yup.string().min(6).required("ID é um campo obrigatório"),
     address: Yup.object().shape({
-        city: Yup.string().min(2).required().label("city"),
-        street: Yup.string().min(2).required().label("street"),
+        city: Yup.string().min(2).required("Cidade é um campo obrigatório").label("city"),
+        street: Yup.string().min(2).required("Rua é um campo obrigatório").label("street"),
     })
 })
